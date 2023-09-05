8fe0799159ec747abe4725d21d69fb36b106cb7c
youssefElkilany <elkilany17@gmail.com>
2023-09-06 00:52:09 +0300

    get users

--- src/modules/user/controller/user.js
+++ src/modules/user/controller/user.js
@@ -1,1 +1,11 @@
 //import userModel from "../../../../DB/model/User.model.js";
+
+import userModel from "../../../../DB/model/User.model.js";
+import { asyncHandler } from "../../../utils/errorHandling.js";
+
+
+export const getallusers = asyncHandler(async(req,res,next)=>{
+
+    const users = await userModel.find()
+    return res.json({users})
+})
